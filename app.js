17a3df6975f9f8859b29994b616819eefa607053
Spacecowboi <cunningham2192@gmail.com>
2023-07-17 14:34:10 -0700

    added all required HTML files, app.js functionality to satisfy lab requirements, still working

--- app.js
+++ app.js
@@ -1,1 +1,69 @@
 "use strict";
+
+function generateRandomCustomers(){ //math.floor function constructed with help from ChatGPT
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+}
+
+const seattle = {
+    minCust: 23,
+    maxCust: 65,
+    avgCookies: 6.3,
+    getRandomCustomers: generateRandomCustomers, 
+    hours: ['6am', '7am', '8am', '9am', '10am', '11am','12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+    cookieSales: [], //Store cookie sales for each hour
+
+projectedCookieSales: function(){
+    for(let i = 0; i < this.hours.length; i++){
+        let customers = this.getRandomCustomers(); // genereating random customers per hour=
+        let cookiesPurchased = customers * this.avgCookies; // Getting amount of cookies purchased
+        this.cookieSales.push(cookiesPurchased); // Putting our previously collected data into our empty array
+    }
+ }
+};
+seattle.getRandomCustomers();
+seattle.projectedCookieSales(); // Calculate AND store sales for each hour open
+console.log('Seattle sales:', seattle.cookieSales);
+
+
+// const customersPerHour = seattle.getRandomCustomersPerHour();
+// console.log("Number of customers per hour:", customersPerHour);
+
+const Tokyo = {
+    minCust: 3,
+    maxCust: 24,
+    avgCookies: 1.2,
+    getRandomCustomers: generateRandomCustomers,
+    hours: ['6am', '7am', '8am', '9am', '10am', '11am','12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+    cookieSales: [],
+};
+Tokyo.getRandomCustomers();
+
+const Dubai = {
+    minCust: 11,
+    maxCust: 38,
+    avgCookies: 3.7,
+    getRandomCustomers: generateRandomCustomers,
+    hours: ['6am', '7am', '8am', '9am', '10am', '11am','12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+    cookieSales: [],
+};
+Dubai.getRandomCustomers();
+
+const Paris = {
+    minCust: 20,
+    maxCust: 38,
+    avgCookies: 2.3,
+    getRandomCustomers: generateRandomCustomers,
+    hours: ['6am', '7am', '8am', '9am', '10am', '11am','12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+    cookieSales: [],
+};
+Paris.getRandomCustomers();
+
+const Lima = {
+    minCust: 2,
+    maxCust: 16,
+    avgCookies: 2.6,
+    getRandomCustomers: generateRandomCustomers,
+    hours: ['6am', '7am', '8am', '9am', '10am', '11am','12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+    cookieSales: [],
+};
+Lima.getRandomCustomers();
